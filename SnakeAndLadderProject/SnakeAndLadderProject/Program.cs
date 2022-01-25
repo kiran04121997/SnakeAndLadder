f1ee02c03e3010110f6a4bec75b76e24833bcad0
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-26 01:09:48 +0530

    adding the OnePlayer

--- SnakeAndLadderProject/SnakeAndLadderProject/Program.cs
+++ SnakeAndLadderProject/SnakeAndLadderProject/Program.cs
@@ -8,9 +8,13 @@ namespace SnakeAndLadderProject
 {
     internal class Program
     {
+        const int POSITION = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to snake ladder problem");
+            Console.WriteLine("POSITION:" +POSITION);   
+        
+            
             Console.ReadLine();
         }
     }
