ec8ae290acee848a334fb63529e2d6fee01635ba
kiran04121997 <bhosalekiran259@gmail.com>
2022-01-26 00:50:27 +0530

    Adding the snakeandladder

---
+++ SnakeAndLadderProject/SnakeAndLadderProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake ladder problem");
+            Console.ReadLine();
+        }
+    }
+}
