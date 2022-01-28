03269b04cfbb718059232cb1ebc8d5c725b21108
Kiran Bhosale <91077158+kiran04121997@users.noreply.github.com>
2022-01-28 23:43:12 +0530

    Upadate
    Upadated

--- SnakeAndLadderProject/SnakeAndLadderProject/SnakeAndLadder.cs
+++ SnakeAndLadderProject/SnakeAndLadderProject/SnakeAndLadder.cs
@@ -9,7 +9,7 @@ namespace SnakeAndLadderProject
     internal class SnakeAndLadder
     {
        
-            public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+            public const int NO_PLAY = 0, SNAKE = 2, LADDER = 1;
             public int StartPoint = 0;
             public int DiceRoll;
             int Option = 0;
